6b34c14a08cdc6220e4beb566a31253cf8b8d999
Ricardo de Almeida Paula <ricardo.paula@mpce.mp.br>
2021-08-19 18:01:09 -0300

    add-card

--- src/components/commons/WrapperProjetos/index.js
+++ src/components/commons/WrapperProjetos/index.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import { Text } from '../../foundation/Text';
 import { Grid } from '../../layout/Grid';
+import Card from './Card';
+
+const projects = [
+  {
+    id: 1,
+    name: 'Alurakut',
+    imageUrl: '/images/alurakut.png',
+    description: 'Aplicação web clone Orkut, é possível fazer login com o GitHub, ver seus amigos e participar de comunidades!',
+    repoLink: 'https://github.com/ricardodepaula/alurakut',
+    appLink: 'https://alurakut-ricardodepaula1.vercel.app/login',
+  },
+  {
+    id: 2,
+    name: 'Instalura',
+    imageUrl: '/images/instalura.png',
+    description: 'Aplicação web clone do Instagram que foi desenvolvida com o objetivo de aplicar os conceitos da JAMStack utilizando Next.js e styled-components',
+    repoLink: 'https://github.com/ricardodepaula/instalura-base',
+    appLink: 'https://instalura-ricardodepaula.vercel.app/',
+  },
+];
 
 export default function WrapperProjetos() {
   return (
-    <Grid.Container
-      marginTop={{
-        xs: '32px',
-        md: '75px',
-      }}
-    >
+    <Grid.Container>
+      <Grid.Row marginTop="32px" marginBottom="32px">
+        <Grid.Col value={12} display="flex" justifyContent="center">
+          <Text variant="titleMD">Meus projetos</Text>
+        </Grid.Col>
+      </Grid.Row>
+      <Grid.Row marginBottom="16px">
+        {projects.map((project) => (
+          <Grid.Col key={project.id} value={{ xs: 12, md: 4 }}>
+            <Card imageUrl={project.imageUrl} title={project.name} />
+          </Grid.Col>
+        ))}
+      </Grid.Row>
       <Grid.Row>
         <Grid.Col
           value={{ xs: 12, md: 5 }}
@@ -52,18 +79,6 @@ export default function WrapperProjetos() {
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
             Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.
           </Text>
-        </Grid.Col>
-        <Grid.Col
-          value={{
-            xs: 12,
-            md: 6,
-          }}
-        >
-          <img
-            alt="Imagem do Instalura"
-            style={{ display: 'block', margin: 'auto' }}
-            src="./images/homepage.png"
-          />
         </Grid.Col>
       </Grid.Row>
     </Grid.Container>
